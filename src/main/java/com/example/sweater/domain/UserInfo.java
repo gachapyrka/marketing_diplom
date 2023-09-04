551bf743bda831f166de6e49a4ffcec80d1e155b
Yauheni Hancharonak <genyalepel9@yandex.ru>
2023-09-04 04:05:31 +0300

    Users entities add. - Dependencies added; - Packages added; - Entities added; - Created configs and services;

---
+++ src/main/java/com/example/sweater/domain/UserInfo.java
@@ -0,0 +1,56 @@
+package com.example.sweater.domain;
+
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.util.Collection;
+import java.util.Collections;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "user_info")
+public class UserInfo implements UserDetails {
+    @Id
+    @Setter(AccessLevel.PROTECTED)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @NotBlank(message = "Логин не может быть пустым")
+    private String username;
+    @NotBlank(message = "Пароль не может быть пустым")
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    private boolean active = true;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(getRole());
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return active;
+    }
+}
