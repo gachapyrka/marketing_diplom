bad9fb50ec2cabe7745695a3afdee42b279b6229
Yauheni Hancharonak <genyalepel9@yandex.ru>
2023-09-09 09:03:57 +0300

    Maint entities added

--- src/main/java/com/example/sweater/MainController.java
+++ src/main/java/com/example/sweater/MainController.java
@@ -8,19 +8,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.Map;
 
 @Controller
-public class GreetingController {
-    @GetMapping("/greeting")
-    public String greeting(
-            @RequestParam(name="name", required=false, defaultValue="World") String name,
-            Map<String, Object> model
-    ) {
-        model.put("name", name);
-        return "greeting";
-    }
+public class MainController {
 
-    @GetMapping
+    @GetMapping("/")
     public String main(Map<String, Object> model) {
-        model.put("some", "hello, letsCode!");
         return "main";
     }
 }
